f5621f35275e65e053304fdfe99601f124bc2546
jpb <jpb.06@outlook.com>
2019-06-19 19:23:38 +0200

    Filesystem subset

---
+++ src/manipulation/filesystem/file.system.ts
@@ -0,0 +1,16 @@
+﻿import * as fs from 'fs-extra';
+
+export async function findRecFile(
+    folderPath: string
+): Promise<string | undefined> {
+
+    let files = await fs.readdir(folderPath);
+
+    let recFiles = files.filter(el => el.endsWith('.rec'));
+
+    if (recFiles.length > 1) {
+        return undefined;
+    }
+
+    return recFiles[0];
+}
