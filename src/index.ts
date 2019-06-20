e25dc1c143b6f47eaa3d653897bfed2c63ee2d1e
jpb <jpb.06@outlook.com>
2019-06-20 20:37:11 +0200

    Amending entry point

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 ﻿import { DalConfiguration as Configuration } from './configuration/dal.configuration';
 import * as Manipulation from './exports/manipulation.export';
+import * as Util from './exports/util.exports';
 import * as Types from './exports/types.export';
-import * as CryptoUtil from './util/crypto.util';
 
-export { Configuration, Manipulation, Types, CryptoUtil };
+export { Configuration, Manipulation, Util, Types };
