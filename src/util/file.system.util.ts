564a27e56864d5b07c07054054ec7e134bc7c252
jpb <jpb.06@outlook.com>
2019-06-20 20:36:00 +0200

    Moving file system to the new util subset

---
+++ src/util/file.system.util.ts
@@ -0,0 +1,19 @@
+﻿import * as fs from 'fs-extra';
+
+export abstract class FileSystem {
+
+    public static async findRecFile(
+        folderPath: string
+    ): Promise<string | undefined> {
+
+        let files = await fs.readdir(folderPath);
+
+        let recFiles = files.filter(el => el.endsWith('.rec'));
+
+        if (recFiles.length > 1) {
+            return undefined;
+        }
+
+        return recFiles[0];
+    }
+}
