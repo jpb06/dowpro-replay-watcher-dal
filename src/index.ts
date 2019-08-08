fdf3ad1027f01a72e917b9c7976629e6c67a373f
jpb <jpb.06@outlook.com>
2019-08-08 19:33:08 +0200

    Amending entry point

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@
 import * as Manipulation from './exports/manipulation.export';
 import * as Util from './exports/util.exports';
 import * as Types from './exports/types.export';
+import * as Business from './exports/business.export';
 
-export { Configuration, Manipulation, Util, Types };
+export { Configuration, Manipulation, Util, Types, Business };
