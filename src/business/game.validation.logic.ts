089733d962e83b57f679ef108f4fc2e23a0acb69
jpb <jpb.06@outlook.com>
2019-08-08 19:31:28 +0200

    Adding game validation and ladder (elo) logic

---
+++ src/business/game.validation.logic.ts
@@ -0,0 +1,136 @@
+﻿import * as fs from 'fs-extra';
+import * as Unzip from 'node-unzipper';
+import { RelicChunkyParser, Types as RelicChunkyTypes } from 'relic-chunky-parser';
+
+import { GameResult, Game } from '../types/persisted.types';
+import { ArchiveHandlingResult, Status } from '../types/business.types';
+import * as FileSystem from '../util/file.system.util';
+import { GamesStore } from '../manipulation/mongodb/stores/ladder/games.store';
+import { setPlayersStats } from './ladder.logic';
+
+export async function ReadGameResultArchive(
+    uuid: string,
+    archivePath: string,
+    workingPath: string,
+    gamesFolderPath: string
+
+): Promise<ArchiveHandlingResult> {
+
+    // Unzipping file in the provided temp directory
+    let extraction: Unzip.Types.UnzipResult = await Unzip.Manipulation.unzip(archivePath, workingPath);
+    if (!extraction.result) return new ArchiveHandlingResult(
+        Status.UnableToUnzip, `Unable to unzip ${uuid}`, '');
+
+    // Locating relic chunky file
+    let recFile = await FileSystem.findRecFile(workingPath);
+    if (recFile === undefined) return new ArchiveHandlingResult(
+        Status.UnableToLocateRecFile, `Unable to locate replay file for ${uuid}`, '');
+
+    // Getting relic chunky file hash
+    let hash = await FileSystem.fileHash(`${workingPath}/${recFile}`);
+    if (hash === undefined) return new ArchiveHandlingResult(
+        Status.UnableToComputeFileHash, `Unable to compute file hash for ${uuid}`, '');
+
+    // Checking if replay exists in db and therefore was already treated
+    let exists = await GamesStore.getByHash(hash);
+    if (exists) {
+        await fs.remove(workingPath);
+        return new ArchiveHandlingResult(
+            Status.AlreadyExistinginGamesStore, `${hash}:${uuid} Already exists in db store`, hash);
+    }
+
+    let gameFolderPath = `${gamesFolderPath}/${hash}`;
+
+    let folderAlreadyExists = await fs.pathExists(gameFolderPath);
+    if (folderAlreadyExists) {
+        console.log(`Folder ${hash} already exists... Cleaning and copying uploaded files`);
+        await fs.remove(gameFolderPath);
+    }
+    await fs.mkdir(gameFolderPath);
+
+    await fs.move(`${workingPath}/${recFile}`, `${gameFolderPath}/${recFile}`);
+    await fs.move(`${workingPath}/result.json`, `${gameFolderPath}/result.json`);
+
+    // Reading json result
+    let gameResult = await FileSystem.readResult(`${gameFolderPath}/result.json`);
+    if (gameResult === undefined) {
+        await fs.remove(gameFolderPath);
+        return new ArchiveHandlingResult(
+            Status.UnableToParseJsonResultFile, `Unable to parse json game result for ${gameFolderPath}`, hash);
+    }
+    gameResult.MapName = gameResult.MapName.toLowerCase();
+
+    // Parsing relic chunky file
+    let parsedResult = await RelicChunkyParser.getReplayData(`${gameFolderPath}/${recFile}`);
+    if (parsedResult === undefined) return new ArchiveHandlingResult(
+        Status.UnableToParseRecFile, `Unable to parse relic chunky file for ${hash}`, hash);
+
+    // Checking if results are matching between json and relic chunky
+    let isMatching = AreResultsMatching(gameResult, parsedResult);
+    if (!isMatching) {
+        await fs.remove(gameFolderPath);
+        return new ArchiveHandlingResult(
+            Status.ResultsDoNotMatch, `Results did not match for ${hash}`, hash);
+    }
+
+    // Should only treat 1vs1
+    if (gameResult.PlayersCount !== 2) {
+        await fs.remove(gameFolderPath);
+        return new ArchiveHandlingResult(
+            Status.InvalidPlayersCount, `Invalid players count for ${hash}`, hash);
+    }
+
+    let game: Game = {
+        Hash: hash,
+        Result: gameResult,
+        PostedToDiscord: false
+    };
+
+    await GamesStore.add(game);
+
+    let playersStats = await setPlayersStats(gameResult.Players[0], gameResult.Players[1]);
+
+    return {
+        status: Status.Success,
+        errorMessage: '',
+        playersStats: playersStats,
+        hash: hash,
+        mapName: game.Result.MapName,
+        duration: game.Result.Duration,
+        modName: parsedResult.modName,
+        modVersion: gameResult.ModVersion
+    };
+}
+
+function AreResultsMatching(
+    jsonResult: GameResult,
+    relicChunkyResult: RelicChunkyTypes.MapData
+): boolean {
+
+    if (jsonResult.ModName !== relicChunkyResult.modName)
+        return false;
+
+    let relicChunkyPlayers = relicChunkyResult.players.filter(el => el.race.length !== 0);
+
+    if (jsonResult.PlayersCount !== relicChunkyPlayers.length)
+        return false;
+
+    for (let i = 0; i < jsonResult.Players.length; i++) {
+
+        if (relicChunkyPlayers.find(el => el.name === jsonResult.Players[0].Name) === undefined)
+            return false;
+    }
+
+    let internalName = relicChunkyResult.internalName.substr(relicChunkyResult.internalName.lastIndexOf('\\') + 1);
+    if (internalName.toLowerCase() !== jsonResult.MapName)
+        return false;
+
+    // This is not reliable
+    console.log('duration in game file', relicChunkyResult.duration);
+    console.log('duration in result json', jsonResult.Duration);
+    //if (Math.round(relicChunkyResult.duration) !== jsonResult.Duration
+    //    && Math.floor(relicChunkyResult.duration) !== jsonResult.Duration)
+    //    return false;
+
+    return true;
+}
---
+++ src/business/ladder.logic.ts
@@ -0,0 +1,135 @@
+﻿import { GamePlayer, Member } from './../types/persisted.types';
+import { MembersStore} from './../manipulation/mongodb/stores/ladder/members.store';
+
+export async function setPlayersStats(
+    player1: GamePlayer,
+    player2: GamePlayer
+): Promise<Array<GamePlayer & Member>> {
+
+    let member1 = await initializePlayerStats(player1);
+    let member2 = await initializePlayerStats(player2);
+
+    calculateEloRatings(member1, member2, player1.IsAmongWinners ? player1.Name : player2.Name);
+
+    await MembersStore.set(member1);
+    await MembersStore.set(member2);
+
+    return [
+        { ...member1, ...player1 },
+        { ...member2, ...player2 }
+    ];
+}
+
+async function initializePlayerStats(
+    player: GamePlayer
+): Promise<Member> {
+
+    let member = await MembersStore.getByName(player.Name);
+
+    if (member) {
+        // Member exists
+
+        let currentRaceStats = member.RaceStats.find(el => el.Race === player.Race);
+        if (!currentRaceStats) {
+            member.RaceStats.push({
+                Race: player.Race,
+                Wins: player.IsAmongWinners ? 1 : 0,
+                losses: player.IsAmongWinners ? 0 : 1
+            });
+        }
+        else {
+            if (player.IsAmongWinners) currentRaceStats.Wins++;
+            else currentRaceStats.losses++;
+        }
+
+        member.FavoriteRace = member.RaceStats.reduce((prev, current) =>
+            (prev.Wins + prev.losses > current.Wins + current.losses) ? prev : current).Race;
+
+        if (player.IsAmongWinners) member.Wins++;
+        else member.Losses++;
+
+    } else {
+        // Member doesn't exist; creating him
+        member = {
+            Name: player.Name,
+            EloRating: 1000,
+            FavoriteRace: player.Race,
+            Wins: player.IsAmongWinners ? 1 : 0,
+            Losses: player.IsAmongWinners ? 0 : 1,
+            RaceStats: [
+                {
+                    Race: player.Race,
+                    Wins: player.IsAmongWinners ? 1 : 0,
+                    losses: !player.IsAmongWinners ? 1 : 0
+                }
+            ]
+        };
+    }
+
+    return member;
+}
+
+export function calculateRatio(
+    wins: number, losses: number
+): number {
+    let ratio;
+    if (wins === 0) {
+        ratio = 0;
+    } else if (losses === 0) {
+        ratio = 100;
+    } else {
+        ratio = Math.round(((wins / (wins + losses)) * 100) * 100) / 100;
+    }
+    return ratio;
+}
+
+export function calculateEloRatings(
+    player1: Member,
+    player2: Member,
+    winnerName: string
+): void {
+
+    let d = calculateD(player1.EloRating, player2.EloRating);
+    let player1GainProbability = 1 / (1 + Math.pow(10, -d / 400));
+
+    let w = (player1.Name === winnerName) ? 1 : 0;
+    let k = setK(player1.EloRating);
+
+    let points = Math.round(k * (w - player1GainProbability));
+
+    console.log(`points : ${points}`);
+
+    player1.EloRating += points;
+    player2.EloRating -= points;
+}
+
+function setK(
+    elorating: number
+): number {
+    // Players below 2100: K-factor of 32 used
+    // Players between 2100 and 2400: K-factor of 24 used
+    // Players above 2400: K-factor of 16 used.
+
+    let k;
+
+    if (elorating < 2100) k = 32;
+    else if (elorating >= 2100 && elorating < 2400) k = 24;
+    else k = 16;
+
+    return k;
+}
+
+function calculateD(
+    eloRating1: number,
+    eloRating2: number
+) {
+    // capping d at 400
+
+    let d = eloRating1 - eloRating2;
+
+    if (Math.abs(d) > 400) {
+        return d > 0 ? 400 : -400;
+    }
+
+    return d;
+}
